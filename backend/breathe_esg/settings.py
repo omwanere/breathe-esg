66ff6843b86ceeb3ac90d365f27868a5b1b482a5
Om Shivaprasad Wanere <omwanere593@gmail.com>
2026-05-24 20:50:39 +0530

    Configure WhiteNoise and collectstatic to serve Django static files in production

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # CorsMiddleware must be placed before common middleware
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
